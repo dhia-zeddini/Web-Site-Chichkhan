678c7af9cb5aa88b10d4ee93a4103ab942559f64
dhia-zeddini <80212396+dhia-zeddini@users.noreply.github.com>
2023-05-07 19:42:49 +0100

    blog article

---
+++ BlogArticle.php
@@ -0,0 +1,130 @@
+<link rel="stylesheet" href="./global.css" />
+<link rel="stylesheet" href="./BlogArticle.css" />
+<div class="blog-article" style="width:98%">
+
+  <div class="frame-parent">
+    <div class="frame-wrapper">
+      <div class="frame-group" style="width:98%">
+        <div class="frame-container">
+          <div class="article-title-about-jewelry-wrapper">
+            <div class="article-title-about">
+              Article title about jewelry
+            </div>
+          </div>
+          <div class="article-title-about-jewelry-wrapper">
+            <div class="article-title-about">
+              key to chain adaptation in nabel
+            </div>
+          </div>
+        </div>
+        <div class="frame-div" style="width:81.5%">
+          <div class="frame-wrapper1">
+            <div class="frame-parent1">
+              <div class="calendar-parent">
+                <img class="calendar-icon" alt="" src="./public/calendar.svg" />
+
+                <div class="div">15 /04/2023</div>
+              </div>
+              <div class="calendar-parent">
+                <img class="calendar-icon" alt="" src="./public/eye.svg" />
+
+                <div class="div">301</div>
+              </div>
+            </div>
+          </div>
+          <div class="frame-parent2">
+            <div class="tag-wrapper">
+              <div class="article-title-about">Tag</div>
+            </div>
+            <div class="tag-wrapper">
+              <div class="article-title-about">Tag</div>
+            </div>
+            <div class="tag-wrapper">
+              <div class="article-title-about">Tag</div>
+            </div>
+            <div class="tag-wrapper">
+              <div class="article-title-about">Tag</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="support-for-climate-adaptation-parent" style="width:98%">
+      <div class="support-for-climate-container" style="width:85%">
+        <p class="support-for-climate">
+          Support for climate adaptation in Africa is crucial, UN
+          Secretary-General António Guterres said on Tuesday in appealing
+          for greater action to provide renewable energy to hundreds of
+          millions who still lack access to reliable and affordable
+          electricity.
+        </p>
+        <p class="support-for-climate">
+          “As the continent that has contributed least to the climate
+          crisis, Africa deserves the strongest possible support and
+          solidarity”, he told an online dialogue for leaders convened by
+          the African Development Bank. Mr. Guterres warned that “adaptation
+          must not be the neglected half of the climate equation”.
+        </p>
+      </div>
+      <div class="old-models-failing-parent">
+        <div class="article-title-about">Old models failing</div>
+        <div class="although-africa-has-container" style="width:85%">
+          <p class="support-for-climate">
+            Although Africa Has Abundant And Untapped Renewable Resources,
+            It Has Received Just Two Per Cent Of Global Investment In
+            Renewable Energy
+          </p>
+          <p class="support-for-climate">
+            Over The Past Decade, He Reported. Old Models Of Development And
+            Energy Use Have Failed To Provide Africans With Universal Energy
+            Access,
+          </p>
+          <p class="support-for-climate">
+            He Said, Meaning Hundreds Of Millions Of People Still Lack
+            Reliable And Affordable Electricity Or Are Cooking With
+            Polluting And Harmful Fuels.
+          </p>
+          <p class="support-for-climate">
+            “We Can Provide Universal Access To Energy In Africa Primarily
+            Through Renewable Energy. I Call For A Comprehensive Package Of
+            Support To Meet
+          </p>
+          <p class="support-for-climate">
+            This Objective Ahead Of COP26,” Mr. Guterres Said, Referring To
+            The UN Climate Change Conference In November.“It Is Achievable.
+            It Is Necessary. It Is Overdue. And It Is Smart: Climate Action
+            Is A $3 Trillion Investment Opportunity In Africa By 2030,” He
+            Added. However, the Secretary-General pointed to “the major
+            finance” gap blocking progress towards this goal. He urged
+            developed countries to deliver on their $100 billion climate
+            commitment made over a decade ago.“Developed countries and main
+            financers must ensure a swift shift of the billions to support
+            African green investments, to increase resilience and to create
+            the conditions for scaled-up private finance”, he said.“And the
+            private sector must step up and get organized to provide
+            immediate, concrete solutions to governments. Local authorities
+            can work with unions and community leaders on reskilling and
+            social security nets.”
+          </p>
+        </div>
+      </div>
+      <div class="old-models-failing-parent">
+        <div class="article-title-about">Commitment and fiscal autonomy</div>
+        <div class="although-africa-has-container" style="width:85%">
+          <p class="support-for-climate">
+            While African Governments also can lead the way by committing to ambitious adaptation and mitigation plans, they first need to regain
+            their fiscal autonomy, he said. The Secretary-General stressed the need to extend the debt moratorium for developing countries, made last year
+            in response to the COVID-19 pandemic, and even cancelling debts where appropriate. Furthermore, Special Drawing Rights, a type of supplementary
+            foreign reserve maintained by the International Monetary Fund (IMF), must also be made available to support Africa’s recovery.
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+<?php
+include "header.php";
+// include "footer.php";
