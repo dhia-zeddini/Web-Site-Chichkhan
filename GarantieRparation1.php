d142cdf5f360497516352a88bbe3ac0193d47de0
dhia-zeddini <80212396+dhia-zeddini@users.noreply.github.com>
2023-05-07 19:42:03 +0100

    garantie repartition+remboursement et change

---
+++ GarantieRparation1.php
@@ -0,0 +1,126 @@
+<link rel="stylesheet" href="./global.css" />
+<link rel="stylesheet" href="./RemboursementEtChange.css" />
+
+<body>
+  <div class="remboursement-et-change" style="width:98%;height: 2100px;">
+    <div class="remboursement-et-change-inner" style="width:98%">
+      <div class="frame-parent37" style="width:98%">
+        <div class="title-parent1">
+          <div class="les-remboursements-seront-container" style="width:85%">
+            <span>Comme revendeur nous sommes, par la loi, responsables de la garantie légale de nos produits.
+              Cela signifie que nous sommes les premiers responsables pour la garantie sur le produit, même si le fabricant
+              offre une garantie dans le monde entier. S'il vous plaît vérifiez le livret de garantie pour obtenir des informations
+              sur ce qui est couvert par la garantie. Conservez ce manuel dans un endroit sûr, pour savoir où le trouver dans le cas
+              où vous en avez besoin.</span>
+
+          </div>
+        </div>
+
+        <div class="title-parent1">
+          <div class="title4" style="font-weight: 600;">Orientations communes en matière de garantie et de réparation :</div>
+          <div class="les-produits-retourns-container">
+            <ul class="les-produits-retourns-doivent">
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span>En général, la garantie d'usine couvre les défauts dans les matériaux et le mouvement.
+                  La garantie d'usine ne couvre pas les dommages causés par un usage quotidien ou d'utilisation excessive ou abusive des bracelets, des appendices et du verre.
+                </span>
+              </li>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span> La garantie ne couvre pas les dommages faites par l'eau lorsque la montre n'est pas utilisé selon les directives du fabricant.
+                </span>
+              </li>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span>Toutes les montres sont expédies les jours ouvrables. La garantie ne couvre pas la vie de la batterie et/ou le remplacement de la batterie.
+                </span>
+              </li>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span>La garantie est généralement nulle si des réparations non autorisées ont été effectuées par une personne autre qu'un distributeur agréé de la marque.</span>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+        <div class="title-parent1">
+          <div class="title4" style="font-weight: 600;">Réparations sous garantie :</div>
+          <div class="les-remboursements-seront-container" style="width:85%">
+            Pour toutes les marques dont nous sommes revendeur autorisé, vous recevrez une garantie valable dans le monde entier.
+            Il s'agit de la même garantie que vous recevrez de tout revendeur officiel de cette marque. Nous fournissons également une
+            assistance si vous avez besoin de faire une demande de garantie. Dans tous les cas, la montre sera vérifiée et réparé par un
+            horloger agréé ou par le fabricant lui-même.
+            Dans le cas où vous désirez les réparations sous garantie, nous vous demandons de bien vouloir nous contacter par Email ou par
+            téléphone présent dans la page contact. Si le problème que vous rencontrez avec votre montre est couvert par la garantie, nous
+            enverrons la montre à l'usine. Si les travaux d'entretien requis ne sont pas couverts par la garantie, nous vous ferons savoir
+            dès que possible. Dans la plupart des cas, le délai de réparation est d'environ 3 à 4 semaines en fonction de la disponibilité
+            des pièces de rechange.
+          </div>
+        </div>
+        <div class="title-parent1">
+          <div class="title4" style="font-weight: 600;font-size: 30px;">Condition de livraison :</div>
+          <div class="les-remboursements-seront-container" style="width:85%">
+
+            <span>Nous faisons des expéditions du lundi au samedi sauf les jours fériés. Toutes les commandes sont traitées le jour ouvrable
+              de la réception de votre commande
+            </span>
+
+          </div>
+          <div class="les-produits-retourns-container">
+            <ul class="les-produits-retourns-doivent">
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span style="font-weight: 500;font-size: 20px;font-weight: 600;">
+                  Méthodes de livraison :</span>
+                <span>Nous vous offre trois méthodes de livraison, la première via la poste Tunisienne avec un envoi de colis Rapid Poste, la deuxième via une société de transporteur ARAMEX qui fait la livraison à domicile (vous pouvez également récupérer votre colis du Local ARAMEX et dans certain cas nous livrons via notre livreur. Pour les envois en dehors de la Tunisie, nous expédions via un colis DHL Express
+                </span>
+              </li>
+              <br>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span style="font-weight: 500;font-size: 20px;font-weight: 600;">
+                  Coûts de livraison :</span>
+                <span>Les frais de livraison sont calculés automatiquement lors de la commande et sont présentés avant d'effectuer votre achat. Nous offrons la livraison gratuite* dans les conditions suivantes :*Livraison gratuite pour les commandes dont le pays destinataire est la Tunisie et dont le montant totale dépasse 300 Dinars Tunisiens. Chaque fois que ç’est possible, nous vous offrons la plus sûre et la plus rapide des méthodes d'expédition, même si cela signifie un coût plus élevé de l'expédition.
+                </span>
+              </li>
+              <br>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span style="font-weight: 500;font-size: 20px;font-weight: 600;">
+                  Délai de livraison :</span>
+                <span>Nous expédions les colis dès le premier jour ouvrable après la réception de votre commande. Si vous commandez avant 12H00 du lundi au samedi, nous traitons votre commande et expédions votre colis dans la journée. Vous recevez votre colis en 24 Heures à compter de son expédition. Notez que toute expédition hors la Tunisie est estimée de 3 à 7 Jours.
+                </span>
+              </li>
+              <br>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span style="font-weight: 500;font-size: 20px;font-weight: 600;">
+                  Dommages subis lors de l'expédition :</span>
+                <span>Nous sommes responsable de votre commande jusqu'à ce qu'elle vous est livré nouvelle et dans des parfaites conditions de fonctionnement. Lisez les sections ci-dessous sur «article perdu pendant le transport» et «article défectueux ou endommagé pendant le transport» pour plus d'informations sur ce qu'il faut faire dans de telles situations.
+                </span>
+              </li>
+              <br>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span style="font-weight: 500;font-size: 20px;font-weight: 600;">
+                  Article perdu lors de l'expédition :</span>
+                <span>Si votre commande n'est pas arrivée après 3 jours ouvrables, à compter de la date d'expédition, simplement contactez-nous et nous nous occuperons du reste.
+                </span>
+              </li>
+              <br>
+              <li class="conformment-la-loi-nous-de" style="width:85%;font-size: 20px;">
+                <span style="font-weight: 500;font-size: 20px;font-weight: 600;">
+                  Article défectueux ou endommagé pendant le transport :</span>
+                <span>Un article est-il arrivé endommagé ou défectueux ? Nous nous excusons pour la gêne occasionnée. On vous prie de nous contacter pour signaler votre problème et l'inclure dans votre paquet de retour. À la réception de l'article retourné, nous allons prendre soin de remplacer votre achat aussi vite que nous le pouvons. Si ce n'est pas possible de remplacer votre article, vous recevrez un remboursement pour le montant total qui a été payé pour le produit, dans le même mode de paiement et compte à l'origine utilisé pour l'achat.</span>
+              </li>
+              <br>
+
+
+            </ul>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+  </div>
+</body>
+
+</html>
+<?php
+include "header.php";
+// include "footer.php";
---
+++ RemboursementEtChange.php
@@ -0,0 +1,74 @@
+<link rel="stylesheet" href="./global.css" />
+<link rel="stylesheet" href="./RemboursementEtChange.css" />
+
+  <body>
+    <div class="remboursement-et-change" style="width:98%">
+      <div class="remboursement-et-change-inner"style="width:98%">
+        <div class="frame-parent37"style="width:98%">
+          <div class="title-parent1">
+            <div class="title4" style="font-weight: 600">Remboursements</div>
+            <div class="les-remboursements-seront-container"style="width:85%">
+              <span
+                >Les remboursements seront effectués dès que possible après
+                réception de votre colis. S'il vous plaît, permettez 2 à 3 jours
+                pour le traitement de tous les remboursements. Dans tous les
+                cas, les remboursements seront faits sous la forme du paiement
+                d'origine. Tous les retours et échanges issue d’une commande via
+                notre site web doivent être envoyés à l'adresse ci-dessous
+                :</span
+              >
+              <span style="font-weight: 500;font-size: 20px;">
+                Société SFBHR08 Immeuble El Jarra, Av Ali Belhoueine Place la
+                Jarre 8000 Nabeul - Tunisie</span
+              >
+            </div>
+          </div>
+          <div class="title-parent1">
+            <div class="title4"style="font-weight: 600;">Echanges</div>
+            <div class="les-remboursements-seront-container"style="width:85%">
+              Si un produit reçu ne vous donne pas entière satisfaction, vous
+              pouvez bien entendu nous le retourner. Vous avez 5 jours pour nous
+              faire un retour, à compter de la date de réception de votre colis.
+              Si vous nous retournez votre colis dans les 5 jours suivant sa
+              réception, vous pouvez choisir entre un échange, un avoir, ou un
+              remboursement. Dans ce dernier cas, Bijouterie Chichkhane vous
+              remboursera sous 2 à 3 jours (à compter de la date de réception du
+              retour de votre colis).
+            </div>
+          </div>
+          <div class="title-parent1">
+            <div class="title4"style="font-weight: 600;">Conditions d’acceptation du retour</div>
+            <div class="les-produits-retourns-container">
+              <ul class="les-produits-retourns-doivent">
+                <li class="conformment-la-loi-nous-de"style="width:85%;font-size: 22px;">
+                  <span
+                    >les produits retournés doivent être dans un parfait état de
+                    revente, repliés dans leur emballage d’origine avec tous les
+                    accessoires éventuels.</span
+                  >
+                </li>
+                <li class="conformment-la-loi-nous-de"style="width:85%;font-size: 22px;">
+                  <span
+                    >conformément à la loi, nous demandons à nos clients de nous
+                    retourner leurs articles dans des conditions qui nous
+                    permettent de les remettre en vente.</span
+                  >
+                </li>
+              </ul>
+              <p class="contactez-nous-pour-plus-dinf"style="width:85%">
+                <span
+                  >Contactez-nous pour plus d’informations concernant le retour
+                </span>
+                <span class="tl-97" style="font-weight: 550;font-size: 22px;">Tél : 97 507 510 / 72 231 579</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </body>
+</html>
+<?php
+include "header.php";
+include "footer.php";
