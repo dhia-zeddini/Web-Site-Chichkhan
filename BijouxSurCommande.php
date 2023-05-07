f2f61113bd2046455f5f47c38ca69578fb0903f5
dhia-zeddini <80212396+dhia-zeddini@users.noreply.github.com>
2023-05-07 01:23:51 +0100

    service+bijoux sur commande+promotion

---
+++ BijouxSurCommande.php
@@ -0,0 +1,224 @@
+<link rel="stylesheet" href="./global.css" />
+<!-- <link rel="stylesheet" href="./joaillerie.css" /> -->
+<link rel="stylesheet" href="./Promotions.css" />
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" />
+<div>
+  <div class="body">
+    <div class="inner-body">
+      <div class="inner-nav">
+        <div class="filtrer-par">
+          <div class="filtrer-par1">Filtrer par :</div>
+          <div class="filtrer-par2">
+            <div class="filtrer-par2-element" style="width:80px">
+              <div class="catgorie">Catégorie</div>
+              <img alt="" src="./public/vector34.svg" />
+            </div>
+            <div class="filtrer-par2-element" style="width:80px">
+              <div class="catgorie">Metal</div>
+              <img alt="" src="./public/vector34.svg" />
+            </div>
+            <div class="filtrer-par2-element" style="width:80px">
+              <div class="catgorie">Couleur</div>
+              <img alt="" src="./public/vector34.svg" />
+            </div>
+            <div class="filtrer-par2-element" style="width:80px">
+              <div class="catgorie">Sertissage</div>
+              <img alt="" src="./public/vector34.svg" />
+            </div>
+
+
+            <div class="filtrer-par2-element" style="width:80px">
+              <div class="catgorie">Genre</div>
+              <img alt="" src="./public/vector34.svg" />
+            </div>
+
+          </div>
+          <div class="filtrer-par3">
+            <div class="trier-par">Trier par :</div>
+            <div class="recommandations-parent">
+              <div class="recommandations">Recommandations</div>
+              <img alt="" src="./public/vector36.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="pagination-top">
+          <div class="pagination-top1">
+            <div class="afficher">Affichés 1 - 60 sur 362</div>
+            <div class="tout-voir">Tout voir</div>
+          </div>
+          <!-- <div class="pagination-top2">
+                        <div class="vector-wrapper134">
+                            <img class="icon-or-jaune" alt="" src="./public/vector37.svg" />
+                        </div>
+                        <div class="or-jaune">Or jaune</div>
+                    </div> -->
+
+        </div>
+        <div class="inner-container">
+          <div class="inner-container-ligne">
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-61@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-62@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-63@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-64@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-65@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-66@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+
+          </div>
+          <div class="inner-container-ligne">
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-641@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-651@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cards-promos3@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cards-promos5@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-621@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-631@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+
+          </div>
+          <div class="inner-container-ligne">
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-612@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-662@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-622@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-632@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-652@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+            <div class="cards-promos" style="background-image: url(./public/cardsframe-642@3x.png);background-size: cover;">
+              <div class="sur-commande-wrapper">
+                    <div class="sur-commande">Sur commande</div>
+                  </div>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+      </div>
+    </div>
+    <br>
+    <br>
+    <div class="slider-mobileslider-webpagin3" style="width:30%">
+      <div class="pagination9">
+        <div class="pagination-master-page-lab12">
+          <div class="pagination-master-page-lab13">
+            <div class="content6">
+              <img class="media-icon12" alt="" src="./public/media--icon.svg" />
+
+              <div class="previous3">Previous</div>
+              <img class="media-icon13" alt="" src="./public/media--icon1.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="pagination-master-page-num18">
+        <div class="page-number15">1</div>
+      </div>
+      <div class="pagination-master-page-num19">
+        <div class="previous3">2</div>
+      </div>
+      <div class="pagination-master-page-num18">
+        <div class="page-number15">3</div>
+      </div>
+      <div class="pagination10">
+        <div class="pagination-master-page-num21">
+          <div class="pagination-master-page-num22">
+            <div class="page-number15">4</div>
+          </div>
+        </div>
+      </div>
+      <div class="pagination-master-page-num18">
+        <div class="page-number15">5</div>
+      </div>
+      <div class="pagination9">
+        <div class="pagination-master-page-lab12">
+          <div class="pagination-master-page-lab13">
+            <div class="content6">
+              <img class="media-icon13" alt="" src="./public/media--icon2.svg" />
+
+              <div class="previous3">Next</div>
+              <img class="media-icon12" alt="" src="./public/media--icon3.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<?php
+include "header.php";
+// include "footer.php";
+
+?>
---
+++ Services.php
@@ -0,0 +1,134 @@
+ <link rel="stylesheet" href="./Services.css" />
+
+ <body>
+   <div class="services5">
+     
+     <div class="services5">
+       <div class="title7">Nos services</div>
+       <div class="frame-parent46">
+         <div class="content-card-parent">
+           <div class="content-card">
+             <img class="fixed-height-icon" alt="" src="./public/fixedheight@2x.png" />
+
+             <div class="title-parent5">
+               <div class="title8">Créations et Transformations</div>
+               <div class="sur-rendez-vous-notre-container">
+                 <p>
+                   Sur rendez-vous, notre dessinateur de l'atelier crée un
+                   dessin 3D personnalisé pour vos pierres ramenées de voyage,
+                   bijoux anciens à rénover, ou pour offrir une pierre
+                   précieuse. Devis établi, bijou unique réalisé en environ
+                   quinze jours.
+
+                   Facture détaillée avec poids des pierres et certificat de
+                   gemmologie indépendant pour les diamants de plus de 0,50
+                   carat.
+                 </p>
+               </div>
+             </div>
+           </div>
+           <div class="content-card">
+             <img class="fixed-height-icon" alt="" src="./public/fixedheight1@2x.png" />
+
+             <div class="title-parent5">
+               <div class="title8">Authenticité</div>
+               <div class="sur-rendez-vous-notre-container">
+                 <p>
+                   Confiez-nous vos bijoux anciens, de collection, signés ou
+                   d'occasion endommagés, trop petits ou trop grands. Nos
+                   d'experts en
+                   bijouterie propose des services de restauration, de
+                   polissage, de sertissage, d'ajustement de taille, et de
+                   vérification complète. Obtenez un devis gratuit avant toute
+                   intervention. notre savoir-faire artisanal redonnera à vos
+                   bijoux leur éclat d'origine.
+                 </p>
+               </div>
+             </div>
+           </div>
+           <div class="content-card">
+             <img class="fixed-height-icon" alt="" src="./public/fixedheight2@2x.png" />
+
+             <div class="title-parent5">
+               <div class="title8">Réparation de Bijoux & Montres</div>
+               <div class="sur-rendez-vous-notre-container">
+                 <p>
+                   Chichkhane répare bijoux et montres. Experts en restauration
+                   de bijoux anciens, vintage, signés ou d'occasion. Devis
+                   gratuits pour mise à taille, polissage, sertissage. Services
+                   de réparation de montres incluant piles, étanchéité, verre,
+                   bracelets et entretien. Large choix de pièces de rechange pour
+                   différentes marques. Faites-nous confiance pour des
+                   réparations de qualité.
+                 </p>
+               </div>
+             </div>
+           </div>
+
+         </div>
+         <div class="content-card-parent">
+           <div class="content-card">
+             <img class="fixed-height-icon" alt="" src="./public/tlchargement-1@2x.png" />
+
+             <div class="title-parent5">
+               <div class="title8">Enfilage de Perles </div>
+             
+               <div class="sur-rendez-vous-notre-container">
+                 <p>
+                   Votre collier de perles Akoya ne vous plaît plus ? Trop petit
+                   ? Pas de souci, nous pouvons le rajeunir ! nous pouvons le
+                   transformer en un nouveau collier avec des perles plus
+                   grosses, de même couleur et qualité. Pour un port plus près du
+                   cou ou en sautoir ! Contactez-nous pour obtenir un devis
+                   gratuit et offrez une nouvelle vie à vos perles en les
+                   transformant en un bijou qui vous correspond pleinement !
+                 </p>
+               </div>
+             </div>
+           </div>
+           <div class="content-card">
+             <img class="fixed-height-icon" alt="" src="./public/fixedheight3@2x.png" />
+
+             <div class="title-parent5">
+               <div class="title8">Expertises</div>
+               <div class="sur-rendez-vous-notre-container">
+                 <p>
+                  Nous évaluons la valeur de vos bijoux sur rendez-vous confidentiel dans notre 
+                  centre Nos experts vérifient les poinçons, authentifient les signatures et évaluent 
+                  la qualité, la rareté et l'état de vos bijoux.Nous vous offrons une estimation précise
+                   de la valeur de vos bijoux, basée sur notre expertise. Si vous êtes intéressés, nous pouvons
+                    même vous faire une offre d'achat .
+                 </p>
+               </div>
+             </div>
+           </div>
+           <div class="content-card">
+             <img class="fixed-height-icon" alt="" src="./public/fixedheight4@2x.png" />
+
+             <div class="title-parent5">
+               <div class="title8">Sertissage</div>
+               <div class="sur-rendez-vous-notre-container">
+                 <p>
+                   Le sertissage est une technique de fixation de pierres précieuses sur une monture métallique.
+                    Il peut être réalisé avec des griffes, qui sont des tiges repliées sur la pierre. Le serti à
+                     grain utilise de petits copeaux de métal repliés sur le bord de la pierre. Le serti clos consiste
+                      à entourer la pierre d'une mince plaque de métal. Ces techniques sont couramment utilisées pour 
+                      le sertissage de bijoux.
+                 </p>
+               </div>
+             </div>
+           </div>
+
+
+         </div>
+
+       </div>
+     </div>
+
+   </div>
+ </body>
+
+ </html>
+ <?php
+  include "header.php";
+  include "footer.php";
