5bfe2f4ffda400de0644ac4072f73df09d88bb38
dhia-zeddini <80212396+dhia-zeddini@users.noreply.github.com>
2023-05-09 16:53:01 +0100

    contact+checkout (finale v1)

---
+++ Checkout.php
@@ -0,0 +1,243 @@
+<link rel="stylesheet" href="./global.css" />
+<link rel="stylesheet" href="./Checkout.css" />
+
+
+<div class="container">
+  <div class="input-area">
+    <div class="input-area1">
+      <div class="input-area1-title">
+        Vos information personnelles
+      </div>
+      <div class="inner-input">
+        <div class="inner-input1">
+          <div class="inner-input11">
+            <label for="" class="inner-input-label">Nom complet</label>
+            <input type="text" class="input">
+
+          </div>
+          <div class="radio-area">
+            <div class="radio-area1">
+              <label for="" style="width: 25%;">Civilité :</label>
+              <div class="radio-input">
+                <input type="radio" id="html" name="fav_language" value="HTML">
+                  <label for="html" class="radio-input-label">Mme</label>
+              </div>
+              <div class="radio-input">
+                <input type="radio" id="html" name="fav_language" value="HTML">
+                  <label for="html" class="radio-input-label">Mlle</label>
+              </div>
+              <div class="radio-input">
+                <input type="radio" id="html" name="fav_language" value="HTML">
+                  <label for="html" class="radio-input-label">Mr</label>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+        <div class="inner-input1">
+          <div class="inner-input11" style="width: 40%;">
+            <label for="" class="inner-input-label">Téléphone</label>
+            <input type="text" class="input">
+
+          </div>
+          <div class="inner-input11" style="width: 40%;">
+            <label for="" class="inner-input-label">Email</label>
+            <input type="email" class="input">
+
+          </div>
+
+
+        </div>
+      </div>
+
+    </div>
+    <div class="input-area1">
+      <div class="input-area1-title">
+        Votre adresse personnelles
+      </div>
+      <div class="inner-input">
+        <div class="inner-input1">
+          <div class="inner-input11">
+            <label for="" class="inner-input-label">Adresse</label>
+            <input type="text" class="input">
+
+          </div>
+          <div class="inner-input11" style="width: 10%;">
+            <label for="" class="inner-input-label">Ville</label>
+            <input type="text" class="input">
+
+          </div>
+
+
+        </div>
+        <div class="inner-input1">
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Code postal</label>
+            <input type="text" class="input">
+
+          </div>
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Gouvernorat</label>
+            <select name="" id="" class="input"></select>
+
+          </div>
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Pays</label>
+            <select name="" id="" class="input"></select>
+          </div>
+
+
+        </div>
+        <div class="inner-input1">
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Informations complémentaires</label>
+            <input type="text" class="input">
+
+          </div>
+
+
+
+        </div>
+      </div>
+
+    </div>
+    <div class="input-area1">
+      <div class="input-area1-title">
+        Mode de payement
+      </div>
+      <div class="inner-input">
+        <div class="inner-input1">
+          <div class="inner-input11">
+            <!-- <div class="radio-area"> -->
+            <div class="radio-area1">
+
+              <div class="radio-input" style="width: 100%;">
+                  <label for="html" style="width: 100%;">Paiement à la livraison</label>
+                <input type="radio" id="html" name="fav_language" value="HTML">
+              </div>
+
+
+            </div>
+            <div class="radio-area1">
+
+              <div class="radio-input" style="width: 100%;">
+                  <label for="html" style="width: 100%;"> Paiement en ligne</label>
+                <input type="radio" id="html" name="fav_language" value="HTML">
+              </div>
+
+
+            </div>
+
+          </div>
+
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+  <div class="panier">
+    <div class="panier1">
+      <div class="panier1-CardHorizontal">
+        <div class="product-img" style="background-image: url('./public/frame-107@2x.png');background-size: cover;"></div>
+        <div class="product-details">
+          <div class="product-name">
+            <label for="" class="product-name-label">Product name</label>
+            <a href=""><img src="./public/trash2.svg" alt=""></a>
+          </div>
+          <div class="product-description">
+            Solitaire 3.1 gramme en Or Blanc 18 Carats,750% Sertie Zircon
+          </div>
+          <div class="product-footer">
+            <div class="product-quantite">
+              <button class="quntite-btn">-</button>
+              <label for="" class="quantite-label">1</label>
+              <button class="quntite-btn">+</button>
+            </div>
+            <label for="" class="price-label">0.00Tnd</label>
+          </div>
+
+        </div>
+
+      </div>
+      <div class="panier1-CardHorizontal">
+        <div class="product-img" style="background-image: url('./public/baseimage1@2x.png');background-size: cover;"></div>
+        <div class="product-details">
+          <div class="product-name">
+            <label for="" class="product-name-label">Product name</label>
+            <a href=""><img src="./public/trash2.svg" alt=""></a>
+          </div>
+          <div class="product-description">
+            Solitaire 3.1 gramme en Or Blanc 18 Carats,750% Sertie Zircon
+          </div>
+          <div class="product-footer">
+            <div class="product-quantite">
+              <button class="quntite-btn">-</button>
+              <label for="" class="quantite-label">1</label>
+              <button class="quntite-btn">+</button>
+            </div>
+            <label for="" class="price-label">0.00Tnd</label>
+          </div>
+
+        </div>
+
+      </div>
+      <div class="panier1-CardHorizontal">
+        <div class="product-img" style="background-image: url('./public/baseimage2@2x.png');background-size: cover;"></div>
+        <div class="product-details">
+          <div class="product-name">
+            <label for="" class="product-name-label">Product name</label>
+            <a href=""><img src="./public/trash2.svg" alt=""></a>
+          </div>
+          <div class="product-description">
+            Solitaire 3.1 gramme en Or Blanc 18 Carats,750% Sertie Zircon
+          </div>
+          <div class="product-footer">
+            <div class="product-quantite">
+              <button class="quntite-btn">-</button>
+              <label for="" class="quantite-label">1</label>
+              <button class="quntite-btn">+</button>
+            </div>
+            <label for="" class="price-label">0.00Tnd</label>
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </div>
+    <div class="cmd-resume">
+      <label for="" class="cmd-resume-title">Résumé de la commande</label>
+      <div class="cmd-resume-details">
+        <div class="cmd-resume-details1">
+          <label for="" class="cmd-resume-details1-label">Total hors taxes</label>
+          <label for="" class="cmd-resume-details-value">0.00Tnd</label>
+        </div>
+        <div class="cmd-resume-details1">
+          <label for="" class="cmd-resume-details1-label">TVA</label>
+          <label for="" class="cmd-resume-details-value">0.00Tnd</label>
+        </div>
+        <div class="cmd-resume-details1">
+          <label for="" class="cmd-resume-details1-label">Livraison</label>
+          <label for="" class="cmd-resume-details-value">0.00Tnd</label>
+        </div>
+        <div class="cmd-resume-details2">
+          <label for="" class="price-label" style="width: 50%;">Montant Total TTC</label>
+          <label for="" class="price-label">0.00Tnd</label>
+        </div>
+      </div>
+      <button class="confirmer-cmd">Confirmer la commande</button>
+    </div>
+
+  </div>
+
+</div>
+
+
+<?php
+include "header.php";
+// include "footer.php";
---
+++ Contact.php
@@ -0,0 +1,156 @@
+<link rel="stylesheet" href="./global.css" />
+<link rel="stylesheet" href="./Checkout.css" />
+
+
+<div class="container">
+  <div class="input-area">
+    <div class="input-area1">
+      <div class="input-area1-title">
+        Vos information personnelles
+      </div>
+      <div class="inner-input">
+        <div class="inner-input1">
+          <div class="inner-input11">
+            <label for="" class="inner-input-label">Nom complet</label>
+            <input type="text" class="input">
+
+          </div>
+
+
+        </div>
+        <div class="inner-input1">
+          <div class="inner-input11" style="width: 40%;">
+            <label for="" class="inner-input-label">Téléphone</label>
+            <input type="text" class="input">
+
+          </div>
+          <div class="inner-input11" style="width: 40%;">
+            <label for="" class="inner-input-label">Email</label>
+            <input type="email" class="input">
+
+          </div>
+
+
+        </div>
+      </div>
+
+    </div>
+    <div class="input-area1">
+      
+      <div class="inner-input">
+        <div class="inner-input1">
+          <div class="inner-input11">
+            <label for="" class="inner-input-label">Adresse</label>
+            <input type="text" class="input">
+
+          </div>
+          <div class="inner-input11" style="width: 10%;">
+            <label for="" class="inner-input-label">Ville</label>
+            <input type="text" class="input">
+
+          </div>
+
+
+        </div>
+        <div class="inner-input1">
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Code postal</label>
+            <input type="text" class="input">
+
+          </div>
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Gouvernorat</label>
+            <select name="" id="" class="input"></select>
+
+          </div>
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Pays</label>
+            <select name="" id="" class="input"></select>
+          </div>
+
+
+        </div>
+        <div class="inner-input1">
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Sujet</label>
+            <input type="text" class="input">
+
+          </div>
+
+
+
+        </div>
+        <div class="inner-input1">
+          <div class="inner-input11" style="width: 30%;">
+            <label for="" class="inner-input-label">Sujet</label>
+            <textarea name="" id="" cols="30" rows="10" class="input" style="height: 100px;"></textarea>
+
+          </div>
+
+
+
+        </div>
+      </div>
+
+    </div>
+    <br>
+    <br>
+    <button class="confirmer-cmd">Envoyer</button>
+  </div>
+  <div class="contact-details">
+    <div class="logo">
+      <img src="./public/vector50.svg" alt="" class="logo-img">
+      <img src="./public/vector56.svg" alt="" class="logo-img1">
+    </div>
+    <div class="contact-details1">
+      <div class="inner-details">
+        <span>
+          Nous sommes à votre disposition pour toute Infos ou demande spéciale, vous pouvez nous rendre visite dans nos boutiques .
+
+        </span>
+        <br>
+        <span>
+          du Lundi au Samedi de 8h30 à 19h30.
+        </span>
+
+      </div>
+      <div class="locations">
+        <div class="locations1">
+          <img src="./public/phmappinline.svg" alt="" class="icon">
+          <p class="locations1-label">
+            Nabeul RB5 Avenue Habib Thameur 8000 Nabeul - Tunisie
+          </p>
+
+        </div>
+        <div class="locations1">
+          <img src="./public/phmappinline.svg" alt="" class="icon">
+          <p class="locations1-label">
+            Lac 1 RDC N°09 Immeuble Kenzat, Av Lac Victoria
+            1053 Tunis - Tunisie </p>
+
+        </div>
+        <div class="locations1">
+          <img src="./public/icbaselinemailoutline.svg" alt="" class="icon">
+          <p class="locations1-label">
+            Contact@bijouterie-chichkhane.com </p>
+
+        </div>
+        <div class="locations1">
+          <img src="./public/icoutlinelocalphone.svg" alt="" class="icon">
+          <p class="locations1-label">
+            Téléphone : (+216) 72 231 579 / (+216) 97 507 510 </p>
+
+        </div>
+
+      </div>
+    </div>
+
+
+  </div>
+
+</div>
+
+
+<?php
+include "header.php";
+// include "footer.php";
